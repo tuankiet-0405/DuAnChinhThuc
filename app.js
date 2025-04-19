f039418487c7a5c9b7186a01f7e08166df7c2dff
tuankiet-0405 <tk04052k4@gmail.com>
2025-04-19 11:26:14 +0700

    update

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require('path');
 const authRoutes = require('./routes/auth'); // Đường dẫn đúng tới auth.js
     
 app.use(express.urlencoded({ extended: true })); // Để đọc form POST
-app.use(express.static(path.join(__dirname, 'public'))); // Để load file tĩnh
+app.use('/public',express.static(path.join(__dirname, 'public'))); // Để load file tĩnh
 app.use('/auth', authRoutes); // ⚠️ ĐĂNG KÝ ROUTE auth
-
+app.use('/views' ,express.static(path.join(__dirname, 'views'))); // Để load file tĩnh
 app.listen(3000, () => {
-  console.log('✅ Server chạy tại http://localhost:3000');
+  console.log('✅ Server chạy tại http://localhost:3000/public/index.html');
 });
